b338046250ebee14a55da42fd973f78df763cc73
Miguel Beignon <miguel@twic.pics>
2022-12-22 16:55:33 +0100

    Wires with stackBlitz.

--- src/app/components/twic-wrapper/twic-wrapper.component.ts
+++ src/app/components/twic-wrapper/twic-wrapper.component.ts
@@ -1,32 +1,40 @@
 import { Component, Input, OnInit } from '@angular/core';
 
-const ONLINE_URL = `https://stackblitz.com/edit/github-wpprt7?file=`;
-const GITHUB = `https://github.com/TwicPics/components-demo-angular`;
 
+const GITHUB_REPO_NAME = `TwicPics/components-demo-angular`;
+const GITHUB = `https://github.com/${GITHUB_REPO_NAME}`;
 @Component({
   selector: 'TwicWrapper',
   templateUrl: './twic-wrapper.component.html',
   styleUrls: ['./twic-wrapper.component.scss']
 })
 
+
 export class TwicWrapperComponent implements OnInit {
 
-  @Input() gitHubUrl?: string;
+  @Input() filename?: string;
 
   DOCUMENTATION_URL = `https://www.twicpics.com/docs/components/angular?utm_source=github&utm_medium=organic&utm_campaign=components#angular`;
   FRAMEWORK_LOGO = `https://assets.twicpics.com/demo/@twicpics-components/logos/angular.svg`;
   GIHUB_LOGO = `assets/github-mark-white.svg`;
   STACKBLITZ_LOGO = `assets/stackblitz.svg`;
 
+
+  initialPath?: string;
   onLineUrl = ``;
-  gitHubRedirect = ``;
+  gitHubUrl = ``;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.onLineUrl = `${ONLINE_URL}${this.gitHubUrl || "README.md"}`;
-    this.gitHubRedirect = this.gitHubUrl
-      ? `${GITHUB}/blob/main/${this.gitHubUrl}`
-      : GITHUB;
+    this.gitHubUrl = this.filename ? `${GITHUB}/blob/main/${this.filename}` : GITHUB;
+    this.onLineUrl = `https://stackblitz.com/github/${GITHUB_REPO_NAME}?file=${this.filename || "README.md"}`;
+    if (this.filename) {
+      const test = /twic-(.*)\//.exec(this.filename);
+      if (test) {
+        const [_, initialPath] = test;
+        this.onLineUrl = `${this.onLineUrl}&initialpath=${initialPath}`;
+      }
+    }
   }
 }
